c4d29b09e93321a0dea1b769106740f3c7935171
huchoperryi@gmail.com <huchoperryi@gmail.com>
2018-08-16 07:09:34 +0000

    add control

--- Lifegame.py
+++ Lifegame.py
@@ -126,6 +126,64 @@ class LifegameField():
 		(0,0,0,0,0,0,0,0,0,0, 0,0,0,1,1,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0),
 		(0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0))
 
+
+class PartsFloat(SpriteNode):
+	
+	def __init__(self, data_object, **kwargs):
+		
+		self.mirror = ''
+		self.data_object = data_object
+		#draw object
+		self.blAddObject = LifegameObject.data2bool(data_object).astype(dtype=np.uint8)
+		part_img = Image.fromarray((1 - self.blAddObject) * 128 + 63)
+		pilimgfile = io.BytesIO()
+		part_img.save(pilimgfile, format='png')
+		bytes_img = pilimgfile.getvalue()
+		uiimg = ui.Image.from_data(bytes_img)
+		texture = Texture(uiimg)
+		SpriteNode.__init__(self,
+			texture,
+			position=(100,100),
+			anchor_point=(0,0),
+			z_position=1,
+			**kwargs)
+		self.margin = self.size * 0.4
+	
+	def reverse_h(self, **kwargs):
+		
+		position = self.position
+		
+		self.remove_from_parent()
+		
+		if self.mirror == '':
+			self.mirror = 'h'
+			
+		elif self.mirror == 'h':
+			self.mirror = ''
+			
+		else:
+			print('mirror parameter error ', self.mirror)
+				
+		#draw object
+		self.blAddObject = LifegameObject.data2bool(self.data_object,mirror=self.mirror).astype(dtype=np.uint8)
+		
+		part_img = Image.fromarray((1 - self.blAddObject) * 128 + 63)
+		pilimgfile = io.BytesIO()
+		part_img.save(pilimgfile, format='png')
+		bytes_img = pilimgfile.getvalue()
+		uiimg = ui.Image.from_data(bytes_img)
+		texture = Texture(uiimg)
+		SpriteNode.__init__(self,
+			texture,
+			position=position,
+			z_position=1,
+			**kwargs)
+		self.margin = self.size * 0.4
+		
+	def move_position(self, pos_move):
+		self.position = self.position + pos_move
+	
+		
 class MyScene(Scene):
 	
 	def setup(self):
@@ -204,34 +262,142 @@ class MyScene(Scene):
 			parent=self)
 		
 		#add control areas
-		self.btnEditOnBak = ShapeNode(
-			path=ui.Path.rounded_rect(0,0,50,50,5),
+		self.btn0Bak = ShapeNode(
+			path=ui.Path.rounded_rect(0,0,44,50,5),
 			color='#c4e6ff',
 			anchor_point=(0.5,0.5),
-			position=(25,25),
+			position=(22,25),
 			parent=self)
 		
-		self.btnEditOn = SpriteNode(
+		self.btn0Symbol = SpriteNode(
 			'iob:settings_32',
 			anchor_point=(0.5,0.5),
-			position=(25,25),
+			position=(22,25),
+			z_position=1.0,
+			parent=self)
+		
+		self.btn1Bak = ShapeNode(
+			path=ui.Path.rounded_rect(0,0,44,50,5),
+			fill_color='#a0a0a0',
+			anchor_point=(0.5,0.5),
+			position=(67,25),
+			parent=self)
+		
+		self.btn1Symbol = SpriteNode(
+			'iob:arrow_left_a_32',
+			anchor_point=(0.5,0.5),
+			position=(67,25),
 			z_position=1.0,
 			parent=self)
 			
-		self.btnEditSubmitBak = ShapeNode(\
-			path=ui.Path.rounded_rect(0,0,50,50,5),
+		self.btn2Bak = ShapeNode(
+			path=ui.Path.rounded_rect(0,0,44,50,5),
+			fill_color='#a0a0a0',
+			anchor_point=(0.5,0.5),
+			position=(112,25),
+			parent=self)
+			
+		self.btn2Symbol = SpriteNode(
+			'iob:arrow_down_a_32',
+			anchor_point=(0.5,0.5),
+			position=(112,25),
+			z_position=1.0,
+			parent=self)
+		
+		self.btn3Bak = ShapeNode(
+			path=ui.Path.rounded_rect(0,0,44,50,5),
+			fill_color='#a0a0a0',
+			anchor_point=(0.5,0.5),
+			position=(157,25),
+			parent=self)
+			
+		self.btn3Symbol = SpriteNode(
+			'iob:arrow_up_a_32',
+			anchor_point=(0.5,0.5),
+			position=(157,25),
+			z_position=1.0,
+			parent=self)
+		
+		self.btn4Bak = ShapeNode(
+			path=ui.Path.rounded_rect(0,0,44,50,5),
+			fill_color='#a0a0a0',
+			anchor_point=(0.5,0.5),
+			position=(202,25),
+			parent=self)
+			
+		self.btn4Symbol = SpriteNode(
+			'iob:arrow_right_a_32',
+			anchor_point=(0.5,0.5),
+			position=(202,25),
+			z_position=1.0,
+			parent=self)
+		
+		self.btn5Bak = ShapeNode(
+			path=ui.Path.rounded_rect(0,0,44,50,5),
+			fill_color='#a0a0a0',
+			anchor_point=(0.5,0.5),
+			position=(247,25),
+			parent=self)
+		
+		self.btn5Symbol = SpriteNode(
+			'iob:arrow_swap_32',
+			anchor_point=(0.5,0.5),
+			position=(247,25),
+			z_position=1.0,
+			parent=self)
+			
+		self.btn6Bak = ShapeNode(
+			path=ui.Path.rounded_rect(0,0,44,50,5),
+			fill_color='#a0a0a0',
+			anchor_point=(0.5,0.5),
+			position=(292,25),
+			parent=self)
+		
+		self.btn6Symbol = SpriteNode(
+			'iob:ios7_undo_32',
+			anchor_point=(0.5,0.5),
+			position=(292,25),
+			parent=self)
+			
+		self.btn7Bak = ShapeNode(
+			path=ui.Path.rounded_rect(0,0,44,50,5),
 			fill_color='#c4e6ff',
 			anchor_point=(0.5,0.5),
-			position=(350,25),
+			position=(337,25),
 			parent=self)
 		
-		self.btnEditSubmit = SpriteNode(\
+		self.btn7Symbol = SpriteNode(
 			'iob:arrow_right_b_32',
 			anchor_point=(0.5,0.5),
-			position=(350,25),
+			position=(337,25),
 			parent=self)
 		
-
+	def button0_push(self):
+		pass
+		
+	def button1_push(self):
+		
+		if self.modeEdit == True:
+			#self.parts_node.position = self.parts_node.position + (-1, 0)
+			self.parts_node.move_position((-1, 0))
+		self.posPartX.text = str(self.parts_node.position.x)
+		self.posPartY.text = str(self.parts_node.position.y - self.intLowerMargin)
+	
+	def button2_push(self):
+		self.parts_node.move_position((0, -1))
+	
+	def button3_push(self):
+		self.parts_node.move_position((0, 1))
+	
+	def button4_push(self):
+		self.parts_node.move_position((2, 0))
+	
+	def button5_push(self):
+		self.parts_node.reverse_h(parent=self)
+	
+	def button6_push(self):
+		pass
+	
 	def update(self):
 		if not(self.flgStop):
 			self.field_node.remove_from_parent()
@@ -248,14 +414,18 @@ class MyScene(Scene):
 
 		# SpriteNodeを使い、スクリーンに追加する
 		#self.field_node = SpriteNode(texture, position=self.size/2)
-		self.field_node = SpriteNode(texture, anchor_point=(0,0), position=(0, self.intLowerMargin))
-
-		self.add_child(self.field_node)
+		self.field_node = SpriteNode(
+			texture,
+			anchor_point=(0,0),
+			position=(0, self.intLowerMargin),
+			parent=self)
 		#print(self.field.gen)
 		#print(self.field.gen)
 		
-		self.lblGen.text = 'Gen.: ' + str(self.field.gen)
-		self.lblPoplation.text = 'Poplation: ' + str(self.field.field.sum())
+		self.lblGen.text =\
+			'Gen.: ' + str(self.field.gen)
+		self.lblPoplation.text =\
+			'Poplation: ' + str(self.field.field.sum())
 
 	def touch_began(self, touch):
 		x, y = touch.location
@@ -266,12 +436,16 @@ class MyScene(Scene):
 				self.parts_node.remove_from_parent()
 				self.modeEdit = False
 				self.flgStop = False
-				self.field.mySetObject(self.parts_node.position[0],self.parts_node.position[1] - self.intLowerMargin, self.object_add)
-			if x < 50 and self.modeEdit == False:
+				self.field.mySetObject(self.parts_node.position[0],self.parts_node.position[1] - self.intLowerMargin, self.parts_node.blAddObject)
+			
+			
+			if x < 45 and self.modeEdit == False:
 				
 				self.modeEdit = True
 				self.flgStop = True
 				#draw object
+				self.parts_node = PartsFloat(LifegameObject.Glidergun, parent=self)
+				'''
 				self.object_add = LifegameObject.data2bool(LifegameObject.Glidergun).astype(dtype=np.uint8)
 				part_img = Image.fromarray((1 - self.object_add) * 128 + 63)
 				pilimgfile = io.BytesIO()
@@ -283,9 +457,25 @@ class MyScene(Scene):
 				self.add_child(self.parts_node)
 				
 				self.parts_node.margin = self.parts_node.size * 0.4
+				'''
 				
 				self.modeEdit = True
+			elif 45 < x and x < 90:
+				self.button1_push()
 				
+			elif 90 <= x and x < 135:
+				self.button2_push()
+			
+			elif 135 <= x and x < 190:
+				self.button3_push()
+			
+			elif 190 <= x and x < 225:
+				self.button4_push()
+			
+			elif 225 <= x and x < 270:
+				self.button5_push()
+				
+			
 		else:
 			pass
 			
@@ -312,4 +502,4 @@ class MyScene(Scene):
 
 if __name__ =='__main__':
 
-	run(MyScene(), PORTRAIT, show_fps=True)
+	run(MyScene(), PORTRAIT, show_fps=True, frame_interval=3)
